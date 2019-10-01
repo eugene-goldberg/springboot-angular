729617802b13739261f0181a57d363c9fc4c5afd
Eugene Goldberg <e602684@CHIMAC11592-2.local>
2019-10-01 15:40:30 -0500

    added basic springboot project

---
+++ src/main/java/com/eugene/springbootangular/SpringbootWebAngularApplication.java
@@ -0,0 +1,13 @@
+package com.eugene.springbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebAngularApplication.class, args);
+    }
+
+}
